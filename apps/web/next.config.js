74650699dde27629ac2053c43ee21cb4849dbf45
Manuel Morandin <manuel.morandin@gmail.com>
2026-02-07 11:34:20 +1100

    Initial setup

---
+++ apps/web/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    transpilePackages: [
+        "@finance/ui",
+        "@finance/api-sdk",
+        "@finance/auth-sdk",
+        "@finance/utils",
+    ],
+};
+
+export default nextConfig;
